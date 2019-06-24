52395452bd4fc8674f04135190742a240db12ebe
manish2397 <rawatmanish2397@gmail.com>
2019-06-24 23:56:24 +0530

    questions added

--- Graphs_and_Trees/Trees.cpp
+++ Graphs_and_Trees/Trees.cpp
@@ -5,100 +5,63 @@
 #define dbl double
 using namespace std;
 
-
-class Tree{
-	public:
-	int vertices;
-	list<int> *adjList;
-
-	//Graph Constructor
-	Tree(int v){
-		vertices =v;
-		adjList = new list<int>[vertices];
-	}
-
-
-	//add Edge
-	void addEdge(int a,int b){
-		adjList[a].push_back(b);
-		adjList[b].push_back(a);
-		
-	}
-
-
-	//Print Adjacency List
-	void printAdjList(){
-		for(int i=0;i<vertices;i++){
-			cout<<i<<"-->";
-			for(int x:adjList[i]){
-				cout<<x<<" ";
-			}
-			cout<<endl;
-		}
-	}
-
-	//BFS Traversal
-	void bfs(int startNode){
-		queue<int> q;
-		bool *visited = new bool[vertices]{0};
-		visited[startNode]=1;
-		q.push(startNode);
-		while(!q.empty()){
-			int parent=q.front();
-			cout<<q.front()<<"-->";
-			q.pop();
-			for(int child:adjList[parent]){
-				if(visited[child]==0){
-					q.push(child);
-					visited[child]=1;
-				}
-			}
-		}
-	}
-
-
-	//DFS Traversal
-	void dfs(int startNode,bool visited[]){
-		cout<<startNode<<"-->";
-		visited[startNode]=1;
-		for(int child:adjList[startNode]){
-			if(visited[child]==0)
-				dfs(child,visited);
-		}
-	}
-
-
-	void dfs(int startNode){
-		bool *visited=new bool[vertices]{0};
-		dfs(startNode,visited);
-	}
-
-
+struct Node{
+	int value;
+	Node *left;
+	Node *right;
 };
+
+Node *newNode(int value){
+	Node *node = new Node;
+	node->value=value;
+	node->left=NULL;
+	node->right=NULL;
+	return node;
+}
+
+void preorder(Node *rootNode){
+	if(rootNode==NULL){
+		return;
+	}
+	cout<<rootNode->value<<" ";
+	preorder(rootNode->left);
+	preorder(rootNode->right);
+}
+
+void inorder(Node *rootNode){
+	if(rootNode==NULL){
+		return;
+	}
+	inorder(rootNode->left);
+	cout<<rootNode->value<<" ";
+	inorder(rootNode->right);
+}
+
+void postorder(Node *rootNode){
+	if(rootNode==NULL){
+		return;
+	}
+	postorder(rootNode->left);
+	postorder(rootNode->right);
+	cout<<rootNode->value<<" ";
+}
+
+
 
 
 int main(){
 	
-	Tree t(10);
-	t.addEdge(0,1);
-	t.addEdge(0,2);
-	t.addEdge(1,3);
-	t.addEdge(1,4);
-	t.addEdge(3,5);
-	t.addEdge(5,6);
-	t.addEdge(4,7);
-	t.addEdge(7,8);
-	t.addEdge(4,9);
-	cout<<"Adjacency List : "<<endl;
-	t.printAdjList();
+	Node *rootNode=newNode(4);
+	rootNode->left=newNode(2);
+	rootNode->right=newNode(6);
+	rootNode->left->left=newNode(1);
+	rootNode->right->right=newNode(7);
+	rootNode->left->right=newNode(3);
+	rootNode->right->left=newNode(5);
+	preorder(rootNode);
 	cout<<endl;
-	cout<<"BFS : "<<endl;
-	t.bfs(0);
+	inorder(rootNode);
 	cout<<endl;
-	cout<<"DFS : "<<endl;
-	t.dfs(0);
+	postorder(rootNode);
 	cout<<endl;
-	
-	return 0;
-	
 }
