aaf55426653e746b0101bba04d3ce0a981f391d4
manish2397 <rawatmanish2397@gmail.com>
2019-07-24 22:39:33 +0530

    new questions added

---
+++ codeforces/threePiles.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define lld long long int
+#define ld long int
+#define str string
+#define dbl double
+using namespace std;
+
+
+int main(){
+	lld tc,a,b,c,sum=0;
+	cin>>tc;
+	while(tc--){
+		cin>>a>>b>>c;
+		sum=a+b+c;
+		cout<<sum/2<<endl;
+	}
+}
+
